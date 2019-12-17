9b38a604b6cfae62c24fdeff3f8e15ca1c7d2428
Björn Eistel <bge@mojin-robotics.de>
2019-12-17 16:28:40 +0100

    selftest for new metrics angle and linear displacement and integrated rotation

---
+++ atf_test_app_tf_linear_displacement/scripts/application.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import unittest
+import rospy
+import rostest
+import sys
+
+import atf_core
+from atf_test_tools import PublishTf
+
+class Application:
+    def __init__(self):
+        self.atf = atf_core.ATF()
+        self.ptf = PublishTf()
+
+    def execute(self):
+        self.atf.start("testblock_all")
+
+        # circle
+        self.atf.start("testblock_circle")
+        # FIXME: due to timing problem the first tf message is sometimes omitted
+        #        so next line (pub_zero) is used as a workaround
+        self.ptf.pub_zero(doSleep=True)
+        self.ptf.pub_circ(radius=1, time=10)
+        self.atf.stop("testblock_circle")
+
+        # line
+        self.atf.start("testblock_line")
+        # FIXME: due to timing problem the first tf message is sometimes omitted
+        #        so next line (pub_zero) is used as a workaround
+        self.ptf.pub_zero(doSleep=True)
+        self.ptf.pub_line(length=2, time=5)
+        self.atf.stop("testblock_line")
+
+        # quadrat
+        self.atf.start("testblock_quadrat")
+        # FIXME: due to timing problem the first tf message is sometimes omitted
+        #        so next line (pub_zero) is used as a workaround
+        self.ptf.pub_zero(doSleep=True)
+        self.ptf.pub_quadrat(length=2, time=10)
+        self.atf.stop("testblock_quadrat")
+
+        self.atf.stop("testblock_all")
+
+        # shutdown atf
+        self.atf.shutdown()
+
+if __name__ == '__main__':
+    rospy.init_node('test_app')
+    app = Application()
+    app.execute()
--- atf_test_tools/src/atf_test_tools/publish_tf.py
+++ atf_test_tools/src/atf_test_tools/publish_tf.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 import math
 import sys
+
+import numpy as np
+
 import rospy
 from tf import transformations, TransformBroadcaster
+
 
 class PublishTf:
     def __init__(self):
@@ -13,9 +17,9 @@ class PublishTf:
         self.child2_frame_id = "reference2"
         self.child3_frame_id = "reference3"
         self.child4_frame_id = "reference4"
-        rospy.Timer(rospy.Duration(1 / self.pub_freq), self.reference2)
-        rospy.Timer(rospy.Duration(1 / self.pub_freq), self.reference3)
-        rospy.Timer(rospy.Duration(1 / self.pub_freq), self.reference4)
+        #rospy.Timer(rospy.Duration(1 / self.pub_freq), self.reference2)
+        #rospy.Timer(rospy.Duration(1 / self.pub_freq), self.reference3)
+        #rospy.Timer(rospy.Duration(1 / self.pub_freq), self.reference4)
         rospy.sleep(1.0)
 
     def reference2(self, event):
@@ -31,6 +35,13 @@ class PublishTf:
         self.pub_tf(self.child1_frame_id, self.child4_frame_id, [math.sin(rospy.Time.now().to_sec()),
                                                                  math.cos(rospy.Time.now().to_sec()), 0])
 
+    @staticmethod
+    def rotate(x, y, angle, precision=4):
+        xr = round(x * math.cos(angle) - y * math.sin(angle), precision)
+        yr = round(x * math.sin(angle) + y * math.cos(angle), precision)
+        return xr, yr
+
+
     def pub_tf(self, parent_frame_id, child1_frame_id, xyz=[0, 0, 0], rpy=[0, 0, 0]):
         self.check_for_ctrlc()
         start = rospy.Time.now()
@@ -43,17 +54,27 @@ class PublishTf:
         if (stop-start).to_sec() > 1/self.pub_freq:
             rospy.logwarn("Publishing tf took longer than specified loop rate " + str((stop-start).to_sec()) + ", should be less than " + str(1/self.pub_freq))
 
-    def pub_line(self, length=1, time=1):
+    def pub_line(self, length=1, time=1, same_start_stop_orientation=True):
         rospy.loginfo("Line")
         rate = rospy.Rate(int(self.pub_freq))
 
-        for i in range((int(self.pub_freq * time / 2) + 1)):
+        n_half = int(self.pub_freq * time / 2) + 1
+        for i in range(n_half):
             t = i / self.pub_freq / time * 2
             self.pub_tf(self.parent_frame_id, self.child1_frame_id, [t * length, 0, 0])
             rate.sleep()
-        for i in range((int(self.pub_freq * time / 2) + 1)):
+
+        for i in range(n_half):
+            is_last = i == n_half - 1
             t = i / self.pub_freq / time * 2
-            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [(1 - t) * length, 0, 0])
+            a = math.pi if not is_last and same_start_stop_orientation else 0
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [(1 - t) * length, 0, 0], [0, 0, a])
+            rate.sleep()
+
+    def pub_zero(self, doSleep=False):
+        self.pub_tf(self.parent_frame_id, self.child1_frame_id, [0, 0, 0], [0, 0, 0])
+        if doSleep:
+            rate = rospy.Rate(int(self.pub_freq))
             rate.sleep()
 
     def pub_circ(self, radius=1, time=1):
@@ -62,31 +83,59 @@ class PublishTf:
 
         for i in range(int(self.pub_freq * time) + 1):
             t = i / self.pub_freq / time
-            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [-radius * math.cos(2 * math.pi * t) + radius,
-                                                                     -radius * math.sin(2 * math.pi * t),
-                                                                     0])
+
+            a = 2 * math.pi * t
+            x, y = PublishTf.rotate(0, -radius, a)
+            y += radius
+
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [x, y, 0], [0, 0, a])
             rate.sleep()
 
-    def pub_quadrat(self, length=1, time=1):
+    def pub_quadrat(self, length=1, time=1, same_start_stop_orientation=True):
         rospy.loginfo("Quadrat")
         rate = rospy.Rate(int(self.pub_freq))
 
-        for i in range((int(self.pub_freq * time / 4) + 1)):
-            t = i / self.pub_freq / time * 4
-            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [t * length, 0, 0])
+        distance = np.linspace(0, length * 4, num=int(self.pub_freq*time) + 1)
+
+        xy = np.vstack((distance, np.zeros_like(distance)))
+        a = np.zeros_like(distance)
+
+        alpha = math.pi / 2
+        r = np.round(
+            np.array([
+                [np.cos(alpha), -math.sin(alpha)],
+                [math.sin(alpha), math.cos(alpha)],
+            ]),
+            6
+        )
+
+        sidx = np.argmax(distance > length)
+        xy[:, sidx:] = r.dot(xy[:, sidx:])
+        xy[0, sidx:] += length
+        xy[1, sidx:] -= length
+        a[sidx:] += alpha
+
+        sidx = np.argmax(distance > length * 2)
+        xy[:, sidx:] = r.dot(xy[:, sidx:])
+        xy[0, sidx:] += length*2
+        a[sidx:] += alpha
+
+        sidx = np.argmax(distance > length * 3)
+        xy[:, sidx:] = r.dot(xy[:, sidx:])
+        xy[0, sidx:] += length
+        xy[1, sidx:] += length
+        a[sidx:] += alpha
+
+        if same_start_stop_orientation:
+            a[-1] = a[0]
+
+        xya = np.vstack((xy, a))
+
+        for x, y, a in xya.T:
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [float(x), float(y), 0], [0, 0, float(a)])
             rate.sleep()
-        for i in range((int(self.pub_freq * time / 4) + 1)):
-            t = i / self.pub_freq / time * 4
-            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [length, t * length, 0])
-            rate.sleep()
-        for i in range((int(self.pub_freq * time / 4) + 1)):
-            t = i / self.pub_freq / time * 4
-            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [(1 - t) * length, length, 0])
-            rate.sleep()
-        for i in range((int(self.pub_freq * time / 4) + 1)):
-            t = i / self.pub_freq / time * 4
-            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [0, (1 - t) * length, 0])
-            rate.sleep()
+
+
 
     def check_for_ctrlc(self):
         if rospy.is_shutdown():
